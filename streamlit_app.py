ecf42e217d8cf10d9194167335b3a159dcd7cd97
Aunnasha Sengupta <31107474+Aunnie91@users.noreply.github.com>
2024-02-28 16:26:45 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,39 +1,38 @@
-import streamlit as st 
-import os
-import sys
+import altair as alt
+import numpy as np
+import pandas as pd
+import streamlit as st
 
+"""
+# Welcome to Streamlit!
+Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:.
+If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
+forums](https://discuss.streamlit.io).
+In the meantime, below is an example of what you can do with just a few lines of code:
+"""
 
-from helper_inference_intruder_temp import Intruder_Temp_Inference
+num_points = st.slider("Number of points in spiral", 1, 10000, 1100)
+num_turns = st.slider("Number of turns in spiral", 1, 300, 31)
 
+indices = np.linspace(0, 1, num_points)
+theta = 2 * np.pi * num_turns * indices
+radius = indices
 
+x = radius * np.cos(theta)
+y = radius * np.sin(theta)
 
-def load_image():
- 
-    image_data = st.file_uploader("Upload An Image",type=['png','jpeg','jpg','webp','tif'])
-    if image_data is not None:
-      file_details = {"FileName":image_data.name,"FileType":image_data.type}
-      st.write(file_details)
-      st.image(image_data)
-      with open('/content/drive/MyDrive/Intruder_Detection/Streamlit_App/Input/'+ file_details['FileName'] ,"wb") as f: 
-        f.write(image_data.getbuffer())         
-      st.success("Saved File")
-      if st.button('Run Prediction'):
-        objhelper=Intruder_Temp_Inference()
-        opFilePath=objhelper.run_Inference('/content/drive/MyDrive/Intruder_Detection/Streamlit_App/Input/'+ file_details['FileName'])
-        if(opFilePath=='No Inference'):
-          st.write('Error Something wet wrong')
-        else:
-          st.image(opFilePath)
-        
+df = pd.DataFrame({
+    "x": x,
+    "y": y,
+    "idx": indices,
+    "rand": np.random.randn(num_points),
+})
 
-
-
-
-def main():
-    st.title('Intruder Detection Proof  Of Code')
-    load_image() 
-
-
-
-if __name__ == '__main__':
-    main()
+st.altair_chart(alt.Chart(df, height=700, width=700)
+    .mark_point(filled=True)
+    .encode(
+        x=alt.X("x", axis=None),
+        y=alt.Y("y", axis=None),
+        color=alt.Color("idx", legend=None, scale=alt.Scale()),
+        size=alt.Size("rand", legend=None, scale=alt.Scale(range=[1, 150])),
+    ))
